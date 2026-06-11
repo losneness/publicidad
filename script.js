a0bf6c5933fbdd5907aa3d8134c5dfe82c49cbdc
losneness <lucasarenasillera@angelitos.edu.co>
2026-06-11 11:10:48 -0500

    Update script.js

--- script.js
+++ script.js
@@ -1,19 +1,7 @@
-function mostrarOferta(auto) {
-    let mensaje = "";
-
-    switch(auto) {
-        case "Ferrari":
-            mensaje = "¡Oferta especial! Ferrari F8 con financiamiento en 12 meses sin intereses.";
-            break;
-        case "Lamborghini":
-            mensaje = "¡Oferta de lujo! Lamborghini Huracán con garantía extendida incluida.";
-            break;
-        case "Porsche":
-            mensaje = "¡Imperdible! Porsche 911 con descuento por tiempo limitado.";
-            break;
-        default:
-            mensaje = "¡Consulta nuestras ofertas exclusivas!";
-    }
-
-    alert(mensaje);
+function mostrarOferta(auto){
+    alert(
+        "🔥 Oferta especial para " +
+        auto +
+        "\n\nFinanciación disponible.\nDescuento exclusivo por tiempo limitado.\n¡Contáctanos ahora!"
+    );
 }
